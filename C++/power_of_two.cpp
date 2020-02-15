7c163ba0673282c128fa337b5f7307dcf6c702ee
eric82714 <0958820714ab@gmail.com>
2020-02-15 15:09:51 +0800

    Create power_of_two.cpp

---
+++ C++/power_of_two.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        if(n <= 0) return false;
+        
+        while(n > 2 && n > 0) {
+            if(n % 2 == 1) return false;
+            n /= 2;        
+        }
+        return true;
+    }
+};
